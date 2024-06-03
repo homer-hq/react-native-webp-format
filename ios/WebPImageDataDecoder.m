1c08081ff5e6432d0d70fbdc92eb6393ad63d324
Dariy D <33499565+dariyd@users.noreply.github.com>
2024-06-03 13:26:31 +0200

    Update WebPImageDataDecoder.m

--- ios/WebPImageDataDecoder.m
+++ ios/WebPImageDataDecoder.m
@@ -1,11 +1,13 @@
 #import "WebPImageDataDecoder.h"
 #import <SDWebImageWebPCoder/SDWebImageWebPCoder.h>
+#import "SDImageAPNGCoder.h"
+
 @implementation WebPImageDataDecoder
 
 RCT_EXPORT_MODULE()
 - (BOOL)canDecodeImageData:(NSData *)imageData
 {
-    return [[SDImageWebPCoder sharedCoder] canDecodeFromData:imageData];
+    return [[SDImageAPNGCoder sharedCoder] canDecodeFromData:imageData] || [[SDImageWebPCoder sharedCoder] canDecodeFromData:imageData];
 }
 - (RCTImageLoaderCancellationBlock)decodeImageData:(NSData *)imageData
                                               size:(CGSize)size
@@ -13,7 +15,11 @@ RCT_EXPORT_MODULE()
                                         resizeMode:(RCTResizeMode)resizeMode
                                  completionHandler:(RCTImageLoaderCompletionBlock)completionHandler
 {
-    UIImage *image = [[SDImageWebPCoder sharedCoder] decodedImageWithData:imageData options:nil];
+    uint8_t c;
+    [imageData getBytes:&c length:1];
+    UIImage *image = c == 0x89
+        ? [[SDImageAPNGCoder sharedCoder] decodedImageWithData:imageData options:nil]
+        : [[SDImageWebPCoder sharedCoder] decodedImageWithData:imageData options:nil];
     if (!image) {
       completionHandler(nil, nil);
       return ^{};
