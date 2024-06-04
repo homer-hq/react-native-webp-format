96d5855b81ebdd9a47fc48e0f7e08d23fbc2ce6f
Dariy D <33499565+dariyd@users.noreply.github.com>
2024-06-04 15:35:44 +0200

    Update WebPImageDataDecoder.m

--- ios/WebPImageDataDecoder.m
+++ ios/WebPImageDataDecoder.m
@@ -1,6 +1,11 @@
 #import "WebPImageDataDecoder.h"
 #import <SDWebImageWebPCoder/SDWebImageWebPCoder.h>
 #import "SDImageAPNGCoder.h"
+#import <React/RCTAnimatedImage.h>
+#import <React/RCTUtils.h>
+
+
+#import "RCTImagePlugins.h"
 
 @implementation WebPImageDataDecoder
 
@@ -15,11 +20,25 @@ RCT_EXPORT_MODULE()
                                         resizeMode:(RCTResizeMode)resizeMode
                                  completionHandler:(RCTImageLoaderCompletionBlock)completionHandler
 {
-    uint8_t c;
-    [imageData getBytes:&c length:1];
-    UIImage *image = c == 0x89
-        ? [[SDImageAPNGCoder sharedCoder] decodedImageWithData:imageData options:nil]
-        : [[SDImageWebPCoder sharedCoder] decodedImageWithData:imageData options:nil];
+    
+    
+//    uint8_t c;
+//    [imageData getBytes:&c length:1];
+//    if (c == 0x89) {
+//        RCTAnimatedImage *image = [[RCTAnimatedImage alloc] initWithData:imageData scale:scale];
+//        completionHandler(nil, image);
+//        return ^{};
+//    }
+    
+    // Check if the data is an APNG
+    UIImage *image;
+    if ([[SDImageAPNGCoder sharedCoder] canDecodeFromData:imageData]) {
+        image = [[SDImageAPNGCoder sharedCoder] decodedImageWithData:imageData options:nil];
+    } else {
+        image = [[SDImageWebPCoder sharedCoder] decodedImageWithData:imageData options:nil];
+    }
+    
+    
     if (!image) {
       completionHandler(nil, nil);
       return ^{};
